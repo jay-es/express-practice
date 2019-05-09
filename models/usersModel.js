d8c1bd67b4030ae621b1848ba35a19212f3cf236
Jun Shindo <jayes.mobile@gmail.com>
2019-05-09 10:33:42 +0900

    ユーザー一覧と詳細

---
+++ models/usersModel.js
@@ -0,0 +1,17 @@
+const userData = require('./users-data');
+
+const getSummery = () =>
+  userData.map(v => ({
+    id: v.id,
+    name: v.name,
+  }));
+
+/**
+ * @param {number} id
+ */
+const findById = id => userData.find(v => v.id === id);
+
+module.exports = {
+  getSummery,
+  findById,
+};
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
+const userModel = require('../models/usersModel');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('users', {
+    users: userModel.getSummery(),
+  });
+});
+
+router.get('/:id', function(req, res, next) {
+  const userId = Number(req.params.id);
+  const userData = userModel.findById(userId);
+
+  if (!userData) {
+    next({ message: 'No User Found' });
+  }
+
+  Object.entries(userData).forEach(([key, val]) => {
+    if (val.toString() !== '[object Object]') return;
+
+    // 値がオブジェクトの場合、文字列に変換
+    userData[key] = Object.entries(val)
+      .filter(([k, v]) => typeof v === 'string') // geo 除外
+      .map(([k, v]) => `${k}: ${v}`)
+      .join('<br>');
+  });
+
+  res.render('users-detail', {
+    userData,
+  });
 });
 
 module.exports = router;
